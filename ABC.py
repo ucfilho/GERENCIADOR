6127b9f0bea89b8779a41640487149d11351f2b2
Raiana Roland Seixas <30608466+RaianaSeixas@users.noreply.github.com>
2019-03-12 17:11:31 -0300

    Add files via upload

--- ABC.py
+++ ABC.py
@@ -10,10 +10,9 @@ Original file is located at
 import numpy as np
 from random import randint
 
+ 
+# FOBJ gera vetorialmente os valores com a funcao objetivo escalar
 
-''' 
-FOBJ gera vetorialmente os valores com a funcao objetivo escalar
-'''
 def FOBJ(X,Fun):
     rows = X.shape[0]
     fobj=np.zeros(rows)
@@ -21,7 +20,7 @@ def FOBJ(X,Fun):
         fobj[i]=Fun(X[i,])
     return fobj
 
-'''Enxame retorna a populacao aleatoria com todos valores entre MIN e MAX'''
+# Enxame retorna a populacao aleatoria com todos valores entre MIN e MAX
 def Enxame(PAR,NPAR,MAX,MIN):
     x=np.zeros((NPAR, len(MAX)))
     for j in range(len(MAX)):
@@ -29,10 +28,10 @@ def Enxame(PAR,NPAR,MAX,MIN):
             x[i,j]=MIN[j]+(MAX[j]-MIN[j])*np.random.random()
     return x
 
-''' 
-Fitness gera vetorialmente os valores com a funcao fitness
-equivale a quantidade de nectar da fonte
-'''
+
+# Fitness gera vetorialmente os valores com a funcao fitness
+# Equivale a quantidade de nectar da fonte
+
 def FIT(X,Fun):
     rows = X.shape[0]
     fit=np.zeros(rows)
@@ -44,10 +43,10 @@ def FIT(X,Fun):
             fit[i]=1+abs(fit[i])
     return fit
 
-'''
-probablilidade usada para roleta q atualizacao das onlookers 
-semelhante ao genetico
-'''
+
+# Probablilidade usada para roleta q atualizacao das onlookers 
+#semelhante ao genetico
+
 
 def PROB(x,Fun):
     rows=x.shape[0]
@@ -61,9 +60,9 @@ def PROB(x,Fun):
 
     return prob
 
-'''
-Obtem a melhor escolha greedy search
-'''
+
+#Obtem a melhor escolha greedy search
+
 def GetBest(x,xbest,Fun):
     ycal=FOBJ(x,Fun)
     best=np.argmin(ycal)
@@ -74,9 +73,9 @@ def GetBest(x,xbest,Fun):
         GBEST=x[best,]
     return GBEST
 
-'''
-employed bee phase
-'''
+
+# Employed bee phase
+
 def EmployedBee(xo,x,MAX,MIN,Fun) :
     NPAR = xo.shape[0]
     PAR= xo.shape[1]
@@ -100,9 +99,9 @@ def EmployedBee(xo,x,MAX,MIN,Fun) :
 
     return x
 
-'''
-onlooker bee phase
-'''
+
+# Onlooker bee phase
+
 def OnlookerBee(xo,x,MAX,MIN,Fun): #OnlookerBee(xo,x_Employed)
     rows = xo.shape[0]
     #cols = xo.shape[1]
@@ -110,7 +109,7 @@ def OnlookerBee(xo,x,MAX,MIN,Fun): #OnlookerBee(xo,x_Employed)
     best=np.argmax(prob)
     
     for i in range(rows):
-      #rd=randint(0, (rows-1))
+      rd=randint(0, (rows-1))
       rd=best
       if(prob[rd] > prob[i]):
         fi=np.random.uniform(low=-1.0, high=1.0, size=None)
@@ -119,9 +118,9 @@ def OnlookerBee(xo,x,MAX,MIN,Fun): #OnlookerBee(xo,x_Employed)
     xo=np.clip(xo,MAX,MIN)
     return xo
 
-'''
-scout bee phase
-'''
+
+# Scout bee phase
+
 #def ScoutBee(trial,x,ntrail,MAX,MIN):
 def ScoutBee(x,MAX,MIN):
     PAR = x.shape[0]
@@ -131,15 +130,14 @@ def ScoutBee(x,MAX,MIN):
             x[i,j]=MIN[j]+(MAX[j]-MIN[j])*np.random.random()
     return x
 
-'''
-Metodo que alterna e coordena o uso employers,onlooker, scout bees
-e greedy search
-'''
-def ABCOPtim(ITE,PAR,ntrail,NPAR,MAX,MIN,Fun,xo):
 
-    trial=0 # inicializa contador p abandonar fonte de alimento
-    xbest=Enxame(PAR,1,MAX,MIN)[0,] #inicializa xbest
+# Metodo que alterna e coordena o uso employers,onlooker, scout bees e greedy search
+
+def ABCOPtim(ITE,PAR,NPAR,MAX,MIN,Fun,xo,ntrail):
+
+    trial=0 # inicializa contador p abandonar fonte de alimento 
 #    xo=Enxame(PAR,NPAR,MAX,MIN) # inicializa employed bee-parte 1
+    xbest=Enxame(PAR,1,MAX,MIN)[0,] #inicializa xbest
     vbest_old=Fun(xbest)
     vbest_new=Fun(xbest)
     
@@ -163,9 +161,9 @@ def ABCOPtim(ITE,PAR,ntrail,NPAR,MAX,MIN,Fun,xo):
         FOBEST=Fun(xbest)
         y=FOBJ(x_Employed,Fun)
         XY= np.c_[x_Employed,y] #concatena x e y em 2 colunas            
-        XYsorted = XY[XY[:,-1].argsort()] # Ordena os dados a partir da coluna 2 (Y) para todas as linhas
-
-    return xbest,FOBEST,XYsorted
+        XYsorted = XY[XY[:,-1].argsort()] #Ordena a partir da last col(Y) for all row
+        BEST_XY_ABC=np.append(xbest,FOBEST)
+    return xbest,FOBEST,XYsorted,BEST_XY_ABC
 
 
 ###################### Main
@@ -180,12 +178,12 @@ ITE=300 #ITERACOES
 PAR=2 #NUM DE PARAMETROS A SER OTIMIZADOS
 MAX=[10,10] # MAXIMO DE CADA PARAMETRO
 MIN=[-10,-10] # MINIMO DE CADA PARAMETRO
-ntrail=25 #numero de buscas ate abandonar uma fonte de alimento
+ntrail=10 #numero de buscas ate abandonar uma fonte de alimento
 
 X=Enxame(PAR,NPAR,MAX,MIN) # inicializa employed bee
 
 for i in range(3):
     print("resolucao",i+1," ")
-    xbest,FOBEST,XY=ABCOPtim(ITE,PAR,ntrail,NPAR,MAX,MIN,Fun,X)
+    xbest,FOBEST,XY,BEST_XY_ABC=ABCOPtim(ITE,PAR,NPAR,MAX,MIN,Fun,X,ntrail)
     print("vetor",xbest,"funcao", Fun(xbest),"\n")
 '''    
